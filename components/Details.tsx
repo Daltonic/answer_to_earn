674566be70a3f282791d8f78f3c0c1bd1bfefa43
Daltonic <darlingtongospel@gmail.com>
2023-06-30 14:22:05 +0100

    dynamically grabbing question

---
+++ components/Details.tsx
@@ -0,0 +1,68 @@
+import { BsCalendar3 } from 'react-icons/bs'
+import { FaEthereum } from 'react-icons/fa'
+import { Tags, TagsSm } from './Tags'
+import Identicon from 'react-identicons'
+import React from 'react'
+import { QuestionProp } from '@/utils/interfaces'
+import { getTimeAgo, truncate } from '@/utils/helper'
+
+const Details: React.FC<{ question: QuestionProp }> = ({ question }) => {
+  return (
+    <>
+      <div className="flex flex-col mt-10 space-y-5">
+        <h4
+          className="inline-block from-blue-700 to-pink-700 leading-[30px]
+          bg-gradient-to-r bg-clip-text text-transparent text-[36.65px]"
+        >
+          {question.title}
+        </h4>
+        <p className="text-[14px] leading-[21px] text-[#BBBBBB]">{question.description}</p>
+
+        <div className="sm:hidden flex justify-start items-center space-x-2 text-[#56617B]">
+          <BsCalendar3 size={20} />
+          <span>Asked {getTimeAgo(question.created)}</span>
+        </div>
+
+        <div className="flex justify-between items-center text-[#BBBBBB]">
+          <div className="flex items-center flex-wrap text-[14px]">
+            <div className="hidden sm:flex justify-center items-center space-x-2 text-[#56617B] mr-3">
+              <BsCalendar3 size={20} />
+              <span>Asked {getTimeAgo(question.created)}</span>
+            </div>
+
+            <div className="flex items-center space-x-3 flex-wrap">
+              <div
+                className="flex space-x-1 border border-pink-500 h-[32px] w-[90px]
+                    justify-center items-center rounded-md text-pink-500 cursor-pointer"
+              >
+                <FaEthereum className="w-[10px] h-[15px]" />
+                <p>{question.prize} prize</p>
+              </div>
+
+              <Tags tags={question.tags} />
+              <TagsSm tags={question.tags} />
+            </div>
+          </div>
+
+          <div className="space-x-2 text-xs hidden sm:flex items-center">
+            <Identicon
+              className="h-6 rounded-full bg-slate-600"
+              size={30}
+              string={question.owner}
+            />
+            <p>{truncate({ text: question.owner, startChars: 4, endChars: 4, maxLength: 11 })}</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col space-y-3 text-[#BBBBBB] sm:hidden mt-5">
+        <div className=" space-x-2 text-xs flex items-center">
+          <Identicon className="h-6 rounded-full bg-slate-600" size={30} string={question.owner} />
+          <p>{truncate({ text: question.owner, startChars: 4, endChars: 4, maxLength: 11 })}</p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Details
--- pages/question/[id].tsx
+++ pages/question/[id].tsx
@@ -1,13 +1,13 @@
 import Head from 'next/head'
 import { MdOutlineArrowBackIosNew } from 'react-icons/md'
-import { BsCalendar3, BsFillTrophyFill } from 'react-icons/bs'
+import { BsFillTrophyFill } from 'react-icons/bs'
 import Header from '@/components/Header'
-import { FaEthereum } from 'react-icons/fa'
-import { Tags, TagsSm } from '@/components/Tags'
-import Identicon from 'react-identicons'
 import React from 'react'
+import Details from '@/components/Details'
+import { generateQuestions } from '@/utils/helper'
+import { QuestionProp } from '@/utils/interfaces'
 
-export default function Question() {
+export default function Question({ question }: { question: QuestionProp }) {
   return (
     <div>
       <Head>
@@ -28,7 +28,7 @@ export default function Question() {
             <span className="text-white text-[14px]">Back</span>
           </button>
 
-          <Details />
+          <Details question={question} />
 
           <hr className="bottom border-[#212D4A] w-full my-7" />
 
@@ -49,61 +49,9 @@ export default function Question() {
   )
 }
 
-const Details: React.FC = () => {
-  return (
-    <>
-      <div className="flex flex-col mt-10 space-y-5">
-        <h4
-          className="inline-block from-blue-700 to-pink-700 leading-[30px]
-              bg-gradient-to-r bg-clip-text text-transparent text-[36.65px]"
-        >
-          Event binding on dynamically created elements?
-        </h4>
-        <p className="text-[14px] leading-[21px] text-[#BBBBBB]">
-          I have a bit of code where I am looping through all the select boxes on a page and binding
-          a .hover event to them to do a bit of twiddling with their width on mouse on/off. This
-          happens on page
-        </p>
-
-        <div className="sm:hidden flex justify-start items-center space-x-2 text-[#56617B]">
-          <BsCalendar3 size={20} />
-          <span>Asked 3 months ago</span>
-        </div>
-
-        <div className="flex justify-between items-center text-[#BBBBBB]">
-          <div className="flex items-center flex-wrap text-[14px]">
-            <div className="hidden sm:flex justify-center items-center space-x-2 text-[#56617B] mr-3">
-              <BsCalendar3 size={20} />
-              <span>Asked 3 months ago</span>
-            </div>
-
-            <div className="flex items-center space-x-3 flex-wrap">
-              <div
-                className="flex space-x-1 border border-pink-500 h-[32px] w-[90px]
-                  justify-center items-center rounded-md text-pink-500 cursor-pointer"
-              >
-                <FaEthereum className="w-[10px] h-[15px]" />
-                <p>0.05 prize</p>
-              </div>
-
-              <Tags tags={['Ajax', 'Js']} />
-              <TagsSm tags={['Ajax', 'Js']} />
-            </div>
-          </div>
-
-          <div className="space-x-2 text-xs hidden sm:flex items-center">
-            <Identicon className="h-6 rounded-full bg-slate-600" size={30} string="0x2c...3fe3" />
-            <p>0x2c...3fe3</p>
-          </div>
-        </div>
-      </div>
-
-      <div className="flex flex-col space-y-3 text-[#BBBBBB] sm:hidden mt-5">
-        <div className=" space-x-2 text-xs flex items-center">
-          <Identicon className="h-6 rounded-full bg-slate-600" size={30} string="0x2c...3fe3" />
-          <p>0x2c...3fe3</p>
-        </div>
-      </div>
-    </>
-  )
+export const getServerSideProps = async () => {
+  const data = generateQuestions(1)
+  return {
+    props: { question: JSON.parse(JSON.stringify(data[0])) },
+  }
 }
